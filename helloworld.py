05fb59d61f0369e6e0f379bf9b3c016b6f5511d5
dg189149 <104285952+dg189149@users.noreply.github.com>
2025-12-15 15:29:29 -0500

    Add hello function to print 'Hello world!' multiple times

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+def hello(num):
+  for _ in range(1,num):
+    print(f"{_}: Hello world!"
+
+
+          
+def main():
+  n = int(input("How many times do you want to print 'hello world'?"))
+  hello(n)
+
+main()
